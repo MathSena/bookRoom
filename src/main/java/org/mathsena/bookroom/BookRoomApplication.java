350b5db90a0c4348289e15efcf277dc8830a4abb
MathSena <MathSena07@hotmail.com>
2024-03-20 21:17:18 +0000

    Initial commit

---
+++ src/main/java/org/mathsena/bookroom/BookRoomApplication.java
@@ -0,0 +1,13 @@
+package org.mathsena.bookroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookRoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mathsena/bookroom/BookRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mathsena.bookroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
